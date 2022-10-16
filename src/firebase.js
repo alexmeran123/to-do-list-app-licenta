8ff6fb249ce4ef61c35bd6b3e6cb341343af66fe
alexmeran123 <87770449+alexmeran123@users.noreply.github.com>
2022-10-16 16:10:19 +0300

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  dataBaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: 'AIzaSyDf8iDzroXB0ExRYg8UgOA44uqNNjGAzv0',
+  authDomain: 'todolist-licenta.firebaseapp.com',
+  databaseURL: 'https://todolist-licenta-default-rtdb.firebaseio.com',
+  projectId: 'todolist-licenta',
+  storageBucket: 'todolist-licenta.appspot.com',
+  messagingSenderId: '366562408119',
+  appId: '1:366562408119:web:30839f5c5cdc921e8b3f32',
 })
 
 export { firebaseConfig as firebase }
---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react'
+import { firebase } from '../firebase'
+
+export const useTasks = (selectedProjects) => {
+  const [tasks, setTasks] = useState([])
+}
